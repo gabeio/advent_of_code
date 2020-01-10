69ba3b8d7a5b0e64efcef0d70419cb46c38ea23a
Gabe D <me@gabe.io>
2020-01-10 01:52:00 -0500

    adds step count

--- 2019/3/src/main.rs
+++ 2019/3/src/main.rs
@@ -22,22 +22,25 @@ fn read_line() -> String {
 }
 
 fn follow_wires<'a>(wire1: Vec<&str>, wire2: Vec<&str>) {
-    let grid: HashMap<(i32, i32),i32> = HashMap::new();
-    let grid: HashMap<(i32, i32),i32> = walk_wire(wire1, grid, 1);
-    let grid: HashMap<(i32, i32),i32> = walk_wire(wire2, grid, 3);
+    let grid: HashMap<(i32, i32),(i32,i32,i32)> = HashMap::new();
+    let grid: HashMap<(i32, i32),(i32,i32,i32)> = walk_wire(wire1, grid, 1);
+    let grid: HashMap<(i32, i32),(i32,i32,i32)> = walk_wire(wire2, grid, 3);
+    // println!("{:?}", grid);
     find_closest(&grid);
 }
 
-fn walk_wire(wire: Vec<&str>, grid: HashMap<(i32, i32),i32>, add: i32) -> HashMap<(i32, i32),i32> {
+fn walk_wire(wire: Vec<&str>, grid: HashMap<(i32, i32),(i32,i32,i32)>, add: i32) -> HashMap<(i32, i32),(i32,i32,i32)> {
     let convert = |i: &str| String::from(remove_first(i)).parse().unwrap();
-    let mut grid: HashMap<(i32, i32),i32> = grid;
+    let mut grid: HashMap<(i32, i32),(i32,i32,i32)> = grid;
     let mut cursor: Vec<i32> = vec![0, 0];
+    let mut step: i32 = 0;
     for i in wire {
         let distance: i32 = convert(i);
         let direction: char = i.chars().nth(0).unwrap().try_into().unwrap();
-        let (_grid, _cursor) = walk(cursor, direction, distance, grid, add);
+        let (_grid, _cursor, _step) = walk(cursor, direction, distance, grid, add, step);
         grid = _grid;
         cursor = _cursor;
+        step = _step;
     }
     grid
 }
@@ -52,45 +55,112 @@ fn walk(
     mut cursor: Vec<i32>,
     direction: char,
     distance: i32,
-    mut grid: HashMap<(i32, i32),i32>,
+    mut grid: HashMap<(i32, i32),(i32,i32,i32)>,
     add: i32,
-) -> (HashMap<(i32, i32),i32>, Vec<i32>) {
+    mut step: i32,
+) -> (HashMap<(i32, i32),(i32,i32,i32)>, Vec<i32>, i32) {
     let mut x = cursor[0];
     let mut y = cursor[1];
     for _distance in 0..distance {
+        step += 1;
         if direction == 'R' {
             x += 1;
             cursor[0] += 1;
-            grid.entry((x, y)).and_modify(|e| { *e += add }).or_insert(add);
+            let default: (i32,i32,i32) = if add == 1 {
+                (add,step,0)
+            } else if add == 3 {
+                (add,0,step)
+            } else {
+                (add,0,0)
+            };
+            grid.entry((x, y)).and_modify(|e| {
+                e.0 += add;
+                if add == 1 {
+                    e.1 += step;
+                } else if add == 3 {
+                    e.2 += step;
+                }
+            }).or_insert(default);
         } else if direction == 'L' {
             x -= 1;
             cursor[0] -= 1;
-            grid.entry((x, y)).and_modify(|e| { *e += add }).or_insert(add);
+            let default: (i32,i32,i32) = if add == 1 {
+                (add,step,0)
+            } else if add == 3 {
+                (add,0,step)
+            } else {
+                (add,0,0)
+            };
+            grid.entry((x, y)).and_modify(|e| {
+                e.0 += add;
+                if add == 1 {
+                    e.1 += step;
+                } else if add == 3 {
+                    e.2 += step;
+                }
+            }).or_insert(default);
         } else if direction == 'U' {
             y += 1;
             cursor[1] += 1;
-            grid.entry((x, y)).and_modify(|e| { *e += add }).or_insert(add);
+            let default: (i32,i32,i32) = if add == 1 {
+                (add,step,0)
+            } else if add == 3 {
+                (add,0,step)
+            } else {
+                (add,0,0)
+            };
+            grid.entry((x, y)).and_modify(|e| {
+                e.0 += add;
+                if add == 1 {
+                    e.1 += step;
+                } else if add == 3 {
+                    e.2 += step;
+                }
+            }).or_insert(default);
         } else if direction == 'D' {
             y -= 1;
             cursor[1] -= 1;
-            grid.entry((x, y)).and_modify(|e| { *e += add }).or_insert(add);
+            let default: (i32,i32,i32) = if add == 1 {
+                (add,step,0)
+            } else if add == 3 {
+                (add,0,step)
+            } else {
+                (add,0,0)
+            };
+            grid.entry((x, y)).and_modify(|e| {
+                e.0 += add;
+                if add == 1 {
+                    e.1 += step;
+                } else if add == 3 {
+                    e.2 += step;
+                }
+            }).or_insert(default);
         } else {
             panic!("Unknown {:?}", distance);
         }
     }
-    return (grid, cursor);
+    return (grid, cursor, step);
 }
 
-fn find_closest(grid: &HashMap<(i32, i32),i32>) {
+fn find_closest(grid: &HashMap<(i32, i32),(i32,i32,i32)>) {
     let mut closest = std::i32::MAX;
+    let mut arr: Vec<((i32,i32),(i32,i32,i32))> = Vec::new();
     for (key, value) in grid.iter() {
-        if value == &4i32 || value == &5i32 || value == &7i32 || value == &8i32 {
+        if value.0 == 4i32 || value.0 == 5i32 || value.0 == 7i32 || value.0 == 8i32 {
             if key.0.abs() + key.1.abs() < closest {
                 closest = key.0.abs() + key.1.abs();
             }
+            arr.push((*key,*value));
+        }
+    }
+    let mut smallest: (i32,i32,i32) = arr[0].1;
+    for (_key, value) in arr.iter() {
+        if (value.1 + value.2) < (smallest.1 + smallest.2) {
+            smallest = *value;
         }
     }
     println!("closest: {}", closest);
+    println!("small: {}", smallest.1 + smallest.2);
 }
 
 #[derive(Clone)]
