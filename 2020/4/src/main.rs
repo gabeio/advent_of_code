0945942cc8419e484bdab491e4a9a68134370d22
Gabe De Luca <me@gabe.io>
2020-12-09 00:00:10 +0000

    cleans up extras

--- 2020/4/src/main.rs
+++ 2020/4/src/main.rs
@@ -73,11 +73,9 @@ fn parser(buffer: &String) -> Vec<Passport> {
         let mut passport: Passport = Default::default();
         let one_passport = group.get(0).unwrap().as_str();
         let one_passport: Vec<&str> = one_passport.split(&[' ', '\n'][..]).collect();
-        // 'asdf: 
         for x in one_passport {
             if x != "" {
                 let attribute: Vec<&str> = x.split(':').collect();
-                // println!("{:?} :> {:?}", attribute[0], attribute[1]);
                 match attribute[0] {
                     "byr" => passport.byr = Some(attribute[1].parse::<i16>().unwrap()),
                     "iyr" => passport.iyr = Some(attribute[1].parse::<i16>().unwrap()),
@@ -86,25 +84,13 @@ fn parser(buffer: &String) -> Vec<Passport> {
                     "hcl" => passport.hcl = Some(String::from(attribute[1])),
                     "ecl" => passport.ecl = Some(String::from(attribute[1])),
                     "pid" => passport.pid = Some(attribute[1].to_string()),
-                    // "pid" => {
-                    //     let tmp = match attribute[1].parse::<i64>() {
-                    //         Ok(n) => n,
-                    //         _ => {
-                    //             println!("unable to parse pid");
-                    //             continue 'asdf;
-                    //         },
-                    //     };
-                    //     passport.pid = Some(tmp);
-                    // },
                     "cid" => passport.cid = Some(attribute[1].parse::<i64>().unwrap()),
                     _ => {
-                        // println!("{:?} ::: {:?}", attribute[0], attribute[1])
                     },
                 }
             }
         }
         passports.push(passport);
-        // println!("{:?}", one_passport);
     }
     return passports;
 }
@@ -143,14 +129,12 @@ fn parser_v2(buffer: &String) -> Vec<Passport> {
         'asdf: for x in one_passport {
             if x != "" {
                 let attribute: Vec<&str> = x.split(':').collect();
-                // println!("{:?} :> {:?}", attribute[0], attribute[1]);
                 match attribute[0] {
                     "byr" => {
                         let tmp = attribute[1].parse::<i16>().unwrap();
                         if tmp >= 1920 && tmp <= 2002 {
                             passport.byr = Some(tmp);
                         } else {
-                            // println!("bad byr");
                             continue 'asdf;
                         }
                     },
@@ -159,7 +143,6 @@ fn parser_v2(buffer: &String) -> Vec<Passport> {
                         if tmp >= 2010 && tmp <= 2020 {
                             passport.iyr = Some(tmp);
                         } else {
-                            // println!("bad iyr");
                             continue 'asdf;
                         }
                     },
@@ -168,25 +151,17 @@ fn parser_v2(buffer: &String) -> Vec<Passport> {
                         if tmp >= 2020 && tmp <= 2030 {
                             passport.eyr = Some(tmp);
                         } else {
-                            // println!("bad eyr");
                             continue 'asdf;
                         }
                     },
                     "hgt" => {
                         let tmp = attribute[1];
                         let captures = match re2.captures(tmp) {
-                            Some(n) => {
-                                // println!("n: {:?}", n);
-                                n
-                            },
-                            _ => {
-                                // println!("bad hgt 4");
-                                continue 'asdf;
-                            },
+                            Some(n) => n,
+                            _ => continue 'asdf,
                         };
                         let height: u8 = match captures.get(1) {
                             Some(n) => {
-                                // println!("asdf: {:?}", n);
                                 match n.as_str().parse::<u8>() {
                                     Ok(n) => n,
                                     _ => 0,
@@ -195,77 +170,53 @@ fn parser_v2(buffer: &String) -> Vec<Passport> {
                             _ => 0,
                         };
                         let scale = match captures.get(2) {
-                            Some(n) => { n.as_str() },
+                            Some(n) => n.as_str(),
                             _ => "",
                         };
                         if scale == "in".to_string() {
-                            if height >= 59 && height <= 76 {
-                                //
-                            } else {
-                                // println!("bad hgt 3");
+                            if height >= 59 && height <= 76 {} else {
                                 continue 'asdf;
                             }
                         } else if scale == "cm".to_string() {
-                            if height >= 150 && height <= 193 {
-                                //
-                            } else {
-                                // println!("bad hgt 2");
+                            if height >= 150 && height <= 193 {} else {
                                 continue 'asdf;
                             }
                         } else {
-                            // println!("bad hgt 1");
                             continue 'asdf;
                         }
-                        // println!("{:?}", captures);
                         passport.hgt = Some(String::from(attribute[1]));
                     },
                     "hcl" => {
                         let tmp = attribute[1];
                         let captures = match re3.captures(tmp) {
-                            Some(n) => {
-                                // println!("n: {:?}", n);
-                                n
-                            },
-                            _ => {
-                                // println!("bad hcl");
-                                continue 'asdf;
-                            },
+                            Some(n) => n,
+                            _ => continue 'asdf,
                         };
                         passport.hcl = Some(tmp.to_string());
                     },
                     "ecl" => {
                         let tmp = attribute[1];
-                        if tmp.len() == 3 && ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&tmp) {
-                            //
-                        } else {
-                            // println!("bad ecl");
+                        if tmp.len() == 3 && ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].contains(&tmp) {} else {
                             continue 'asdf;
                         }
                         passport.ecl = Some(tmp.to_string());
                     },
                     "pid" => {
                         if attribute[1].len() != 9 {
-                            // println!("bad pid");
                             continue 'asdf;
                         }
                         let tmp = match attribute[1].parse::<i64>() {
                             Ok(n) => n,
-                            _ => {
-                                // println!("unable to parse pid");
-                                continue 'asdf;
-                            },
+                            _ => continue 'asdf,
                         };
                         passport.pid = Some(tmp.to_string());
                     },
                     "cid" => passport.cid = Some(attribute[1].parse::<i64>().unwrap()),
-                    _ => {
-                        // println!("{:?} ::: {:?}", attribute[0], attribute[1])
-                    },
+                    _ => {},
                 }
             }
         }
         passports.push(passport);
-        // println!("{:?}", one_passport);
     }
     return passports;
 }
