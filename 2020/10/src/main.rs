a5feb1c50f6ec35559c3f560cb39e4863620ebb0
Gabe De Luca <me@gabe.io>
2020-12-13 06:12:55 +0000

    adds WIP day 10 part 2

--- 2020/10/src/main.rs
+++ 2020/10/src/main.rs
@@ -9,28 +9,52 @@ mod tests {
     use std::fs;
 
     #[test]
-    fn input1() -> std::io::Result<()> {
+    fn part1_input1() -> std::io::Result<()> {
         let contents = fs::read_to_string("./input_test1").expect("Can't read file.");
-        let vus: Vec<isize> = contents.split("\n").map(|x| x.parse().unwrap()).collect();
-        assert_eq!(jolt_jumps(vus).unwrap(), 35);
+        let vus: Vec<usize> = contents.split("\n").map(|x| x.parse().unwrap()).collect();
+        assert_eq!(jolt_jumps(vus).unwrap().0, 35);
         Ok(())
     }
 
     #[test]
-    fn input2() -> std::io::Result<()> {
+    fn part1_input2() -> std::io::Result<()> {
         let contents = fs::read_to_string("./input_test2").expect("Can't read file.");
-        let vus: Vec<isize> = contents.split("\n").map(|x| x.parse().unwrap()).collect();
-        assert_eq!(jolt_jumps(vus).unwrap(), 220);
+        let vus: Vec<usize> = contents.split("\n").map(|x| x.parse().unwrap()).collect();
+        assert_eq!(jolt_jumps(vus).unwrap().0, 220);
         Ok(())
     }
 
     #[test]
-    fn input3() -> std::io::Result<()> {
+    fn part1_input3() -> std::io::Result<()> {
         let contents = fs::read_to_string("./input").expect("Can't read file.");
-        let vus: Vec<isize> = contents.split("\n").map(|x| x.parse().unwrap()).collect();
-        assert_eq!(jolt_jumps(vus).unwrap(), 2201);
+        let vus: Vec<usize> = contents.split("\n").map(|x| x.parse().unwrap()).collect();
+        assert_eq!(jolt_jumps(vus).unwrap().0, 2201);
         Ok(())
     }
+
+    #[test]
+    fn part2_input1() -> std::io::Result<()> {
+        let contents = fs::read_to_string("./input_test1").expect("Can't read file.");
+        let vus: Vec<usize> = contents.split("\n").map(|x| x.parse().unwrap()).collect();
+        assert_eq!(jolt_sets(vus).unwrap(), 8);
+        Ok(())
+    }
+
+    #[test]
+    fn part2_input2() -> std::io::Result<()> {
+        let contents = fs::read_to_string("./input_test2").expect("Can't read file.");
+        let vus: Vec<usize> = contents.split("\n").map(|x| x.parse().unwrap()).collect();
+        assert_eq!(jolt_sets(vus).unwrap(), 19208);
+        Ok(())
+    }
+
+    // #[test]
+    // fn part2_input3() -> std::io::Result<()> {
+    //     let contents = fs::read_to_string("./input").expect("Can't read file.");
+    //     let vus: Vec<usize> = contents.split("\n").map(|x| x.parse().unwrap()).collect();
+    //     assert_eq!(jolt_sets(vus).unwrap(), 0);
+    //     Ok(())
+    // }
 }
 
 fn main() -> io::Result<()> {
@@ -50,23 +74,23 @@ fn readin() -> String {
 
 fn part1(buffer: &String) -> std::io::Result<usize> {
     println!("Part 1 Beginning.");
-    let vus: Vec<isize> = buffer.split("\n").map(|x| x.parse().unwrap()).collect();
+    let vus: Vec<usize> = buffer.split("\n").map(|x| x.parse().unwrap()).collect();
     let result = jolt_jumps(vus).unwrap();
     // let result = decrypt(buffer, 25).unwrap().unwrap();
     println!("{:?}", result);
-    Ok(result)
+    Ok(result.0)
 }
 
 fn part2(buffer: &String) -> std::io::Result<usize> {
-    println!("Part 1 Beginning.");
-    let vus: Vec<isize> = buffer.split("\n").map(|x| x.parse().unwrap()).collect();
+    println!("Part 2 Beginning.");
+    let vus: Vec<usize> = buffer.split("\n").map(|x| x.parse().unwrap()).collect();
     let result = jolt_sets(vus).unwrap();
     // let result = decrypt(buffer, 25).unwrap().unwrap();
     println!("{:?}", result);
     Ok(result)
 }
 
-fn jolt_jumps(mut vus: Vec<isize>) -> std::io::Result<usize> {
+fn jolt_jumps(mut vus: Vec<usize>) -> std::io::Result<(usize, usize)> {
     vus.sort();
     println!("vus: {:?}", vus);
     let mut current = 0;
@@ -91,40 +115,55 @@ fn jolt_jumps(mut vus: Vec<isize>) -> std::io::Result<usize> {
     }
     bump_differences(&mut differences, 3);
     println!("differences: {:?}", differences);
-    Ok(differences[&1]*differences[&3])
+    Ok((differences[&1]*differences[&3], current as usize+3))
 }
 
-fn jolt_sets(mut vus: Vec<isize>) -> std::io::Result<usize> {
+fn jolt_sets(mut vus: Vec<usize>) -> std::io::Result<usize> {
     vus.sort();
     println!("vus: {:?}", vus);
     let mut sets: HashSet<Vec<usize>> = HashSet::new();
-    recursive_sets(&mut sets, vus.clone());
-    // loop {
-    //     let mut set: Vec<usize> = vec!();
-    //     loop {
-    //         let mut matches = false;
-    //         let a_match = ofor(1, &vus, current);
-    //         if a_matcher(a_match, &mut differences, &mut current, &mut matches, 1) {
-    //             continue;
-    //         }
-    //         let a_match = ofor(2, &vus, current);
-    //         if a_matcher(a_match, &mut differences, &mut current, &mut matches, 2) {
-    //             continue;
-    //         }
-    //         let a_match = ofor(3, &vus, current);
-    //         if a_matcher(a_match, &mut differences, &mut current, &mut matches, 3) {
-    //             continue
-    //         }
-    //         if !matches {
-    //             break;
-    //         }
-    //     }
-    // }
-    Ok(0)
+    let last = jolt_jumps(vus.clone()).unwrap().1;
+    recursive_sets(&mut sets, last, vus.clone());
+    Ok(sets.len())
 }
 
-fn recursive_sets(sets: &mut HashSet<Vec<usize>>, vus: Vec<isize>) {
-
+fn recursive_sets(sets: &mut HashSet<Vec<usize>>, last: usize, vus: Vec<usize>) {
+    println!("vus: {:?}", vus);
+    let mut set: Vec<usize> = vec!();
+    let mut current = 0;
+    for x in 0..(vus.clone().len()) {
+        if (vus[x]-current) == 1 {
+            set.push(vus[x]);
+            let mut vis = vus.clone();
+            vis.remove(x);
+            recursive_sets(sets, last, vis);
+            current = vus[x];
+            continue;
+        }
+        if (vus[x]-current) == 2 {
+            set.push(vus[x]);
+            let mut vis = vus.clone();
+            vis.remove(x);
+            recursive_sets(sets, last, vis);
+            current = vus[x];
+            continue;
+        }
+        if (vus[x]-current) == 3 {
+            set.push(vus[x]);
+            let mut vis = vus.clone();
+            vis.remove(x);
+            recursive_sets(sets, last, vis);
+            current = vus[x];
+            continue;
+        }
+        break;
+    }
+    if current == last {
+        println!("good set: {:?}", set);
+        sets.insert(set);
+    } else {
+        println!("failed set: {:?}", set);
+    }
 }
 
 fn bump_differences(differences: &mut HashMap<usize,usize>, num: usize) {
