1ea6e79018fe9695c9ee45a55bcf368864888864
Gabe De Luca <me@gabe.io>
2022-12-02 16:06:00 -0500

    adds aoc library

---
+++ src/lib.rs
@@ -0,0 +1,80 @@
+#[macro_use]
+extern crate log;
+
+use std::io::{self, Read};
+
+// grab input as solid string
+fn readin() -> String {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    let _result = handle.read_to_string(&mut buffer);
+    let buffer = buffer.trim();
+    return buffer.to_string();
+}
+
+// split lines
+// split lines by character
+// parse characters into ints
+// collect into grid
+fn int_grid(buffer: &String, split: char) -> Vec<Vec<u32>> {
+    // convert buffer to lines
+    let vstr: Vec<&str> = buffer.split(split).collect();
+    trace!("vstr {:?}", &vstr);
+    let vstr: Vec<Vec<char>> = vstr.iter().map(|e| e.chars().collect()).collect();
+    let convert = |x: char| String::from(x).parse().unwrap();
+    let vconvert = |x: Vec<char>| x.clone().into_iter().map(convert).collect();
+    let vint: Vec<Vec<u32>> = vstr.clone().into_iter().map(vconvert).collect();
+    trace!("vint {:?}", &vint);
+    vint
+}
+
+// split lines
+// split lines by character
+// collect into grid
+fn string_grid(buffer: &String, split: char) -> Vec<Vec<String>> {
+    let vstr: Vec<&str> = buffer.split(split).collect();
+    trace!("vstr {:?}", &vstr);
+    let vstr: Vec<Vec<char>> = vstr.iter().map(|e| e.chars().collect()).collect();
+    let convert = |x: char| String::from(x);
+    let vconvert = |x: Vec<char>| x.clone().into_iter().map(convert).collect();
+    let vstr: Vec<Vec<String>> = vstr.clone().into_iter().map(vconvert).collect();
+    vstr
+}
+
+// split lines
+// convert to ints
+fn int_list(buffer: &String, split: char) -> Vec<u32> {
+    let vstr: Vec<&str> = buffer.split(split).collect();
+    trace!("vstr {:?}", &vstr);
+    let convert = |x: &str| String::from(x).parse().unwrap();
+    let vint: Vec<u32> = vstr.clone().into_iter().map(convert).collect();
+    trace!("vint {:?}", &vint);
+    vint
+}
+
+// split lines
+fn str_list(buffer: &String, split: char) -> Vec<&str> {
+    let vstr: Vec<&str> = buffer.split(split).collect();
+    trace!("vstr {:?}", &vstr);
+    vstr
+}
+
+// split lines
+fn string_list(buffer: &String, split: char) -> Vec<String> {
+    let vstr: Vec<&str> = buffer.split(split).collect();
+    trace!("vstr {:?}", &vstr);
+    let convert = |x: &str| String::from(x);
+    let vstring: Vec<String> = vstr.clone().into_iter().map(convert).collect();
+    vstring
+}
+
+#[cfg(test)]
+mod tests {
+    use super;
+    #[test]
+    fn readin() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
