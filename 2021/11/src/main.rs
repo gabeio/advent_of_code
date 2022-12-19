25d4577e05eebb4322a41359abc4118d8f3e117e
Gabe De Luca <me@gabe.io>
2022-12-19 11:36:39 -0500

    2021 day 11 WIP

--- 2021/11/src/main.rs
+++ 2021/11/src/main.rs
@@ -35,67 +35,73 @@ fn step(board: &mut Vec<Vec<u32>>) -> u32 {
     // including octopuses that are diagonally adjacent.
     // If this causes an octopus to have an energy level greater than 9,
     // it also flashes. This process continues as long as new octopuses keep having their energy level increased beyond 9. (An octopus can only flash at most once per step.)
-    let mut flash = 0;
+    let mut flashes = 0;
     for i in 0..board.len() {
         for j in 0..board[i].len() {
-            if board[i][j] >= 10 {
-                // flash
-                flash += 1;
-                let im1: bool = i != 0;
-                let jm1: bool = j != 0;
-                if im1 {
-                    match board.get_mut(i-1) {
-                        Some(z) => { 
-                            if jm1 {
-                                match z.get_mut(j-1) {
-                                    Some(x) => *x += 1,
-                                    None => panic!("get_mut(j) was none"),
-                                }
-                            }
-                            if let Some(x) = z.get_mut(j) {
-                                *x += 1;
-                            }
-                            if let Some(x) = z.get_mut(j+1) {
-                                *x += 1;
-                            }
-                        },
-                        None => panic!("get_mut(i) was none"),
-                    }
-                }
-                if let Some(z) = board.get_mut(i) {
-                    if jm1 {
-                        match z.get_mut(j-1) {
-                            Some(x) => *x += 1,
-                            None => panic!("get_mut(j) was none"),
-                        }
-                    }
-                    if let Some(x) = z.get_mut(j) {
-                        *x += 1;
-                    }
-                    if let Some(x) = z.get_mut(j+1) {
-                        *x += 1;
-                    }
-                }
-                if let Some(z) = board.get_mut(i+1) {
-                    if jm1 {
-                        match z.get_mut(j-1) {
-                            Some(x) => *x += 1,
-                            None => panic!("get_mut(j) was none"),
-                        }
-                    }
-                    if let Some(x) = z.get_mut(j) {
-                        *x += 1;
-                    }
-                    if let Some(x) = z.get_mut(j+1) {
-                        *x += 1;
-                    }
-                }
-                board[i][j] = 0;
-            }
+            flashes += flash(&mut board, i, j);
         }
     }
     // 3. any octopus that flashed during this step has its energy level set to 0, as it used all of its energy to flash.
-    flash
+    flashes
+}
+
+fn flash(board: &mut Vec<Vec<u32>>, i: usize, j: usize) -> u32 {
+    let mut flashes = 0;
+    if board[i][j] >= 10 {
+        // flash
+        flashes += 1;
+        let im1: bool = i != 0;
+        let jm1: bool = j != 0;
+        if im1 {
+            match board.get_mut(i-1) {
+                Some(z) => { 
+                    if jm1 {
+                        match z.get_mut(j-1) {
+                            Some(x) => *x += 1,
+                            None => panic!("get_mut(j) was none"),
+                        }
+                    }
+                    if let Some(x) = z.get_mut(j) {
+                        *x += 1;
+                    }
+                    if let Some(x) = z.get_mut(j+1) {
+                        *x += 1;
+                    }
+                },
+                None => panic!("get_mut(i) was none"),
+            }
+        }
+        if let Some(z) = board.get_mut(i) {
+            if jm1 {
+                match z.get_mut(j-1) {
+                    Some(x) => *x += 1,
+                    None => panic!("get_mut(j) was none"),
+                }
+            }
+            if let Some(x) = z.get_mut(j) {
+                *x += 1;
+            }
+            if let Some(x) = z.get_mut(j+1) {
+                *x += 1;
+            }
+        }
+        if let Some(z) = board.get_mut(i+1) {
+            if jm1 {
+                match z.get_mut(j-1) {
+                    Some(x) => *x += 1,
+                    None => panic!("get_mut(j) was none"),
+                }
+            }
+            if let Some(x) = z.get_mut(j) {
+                *x += 1;
+            }
+            if let Some(x) = z.get_mut(j+1) {
+                *x += 1;
+            }
+        }
+        board[i][j] = 0;
+    }
+    flashes
 }
 
 fn part1(input: &Vec<Vec<u32>>) -> u32 {
@@ -103,6 +109,7 @@ fn part1(input: &Vec<Vec<u32>>) -> u32 {
     let mut total = 0;
     draw_board(&input);
     for i in 0..100 {
+        plus1(&mut input);
         let steps = step(&mut input);
         total += steps;
         debug!("after step {}: {}", i, steps);
